14781b80add92770005b0c10402335144618f59a
Myron Walker <myron@Myrons-Mac-mini.local>
2024-02-20 23:51:43 -0800

    We should set defaults from the config so environment variables can still have an effect.

--- source/packages/mojo/extension/extensionvariables.py
+++ source/packages/mojo/extension/extensionvariables.py
@@ -25,15 +25,26 @@ from mojo.startup.startupvariables import MOJO_STARTUP_VARIABLES
 
 scloader = StartupConfigLoader("MOJO-EXTENSION")
 
-class MOJO_EXTENSION_VARIABLES(MOJO_STARTUP_VARIABLES):
+
+class MOJO_EXTENSION_VARIABLE_DEFAULTS:
 
 
     MJR_NAME = scloader.get_variable_value("MJR_NAME", default="mjr")
 
-
     MJR_HOME_DIRECTORY = scloader.get_variable_value("MJR_HOME_DIRECTORY",
             default=os.path.expanduser(os.path.join("~", MJR_NAME)))
     MJR_HOME_DIRECTORY = os.path.abspath(os.path.expandvars(os.path.expanduser(MJR_HOME_DIRECTORY)))
+
+    MJR_CONFIGURED_FACTORY_MODULES = scloader.get_variable_value(
+        "MJR_CONFIGURED_FACTORY_MODULES", default=[], converter=CSV_TO_UNIQUE_LIST_CONVERTER
+    )
+
+
+class MOJO_EXTENSION_VARIABLES(MOJO_STARTUP_VARIABLES):
+
+
+    MJR_NAME = MOJO_EXTENSION_VARIABLE_DEFAULTS.MJR_NAME
+    MJR_HOME_DIRECTORY = MOJO_EXTENSION_VARIABLE_DEFAULTS.MJR_HOME_DIRECTORY
 
 
     # For the `MJR_CONFIGURED_FACTORY_MODULES` variable, if we find it in the environment,
@@ -41,9 +52,8 @@ class MOJO_EXTENSION_VARIABLES(MOJO_STARTUP_VARIABLES):
     # then the loader will look in the `MOJO-EXTENSIONS` section of the startup config. If
     # the value is not found in either location, the specifieid default is used.
 
-    MJR_CONFIGURED_FACTORY_MODULES = scloader.get_variable_value(
-        "MJR_CONFIGURED_FACTORY_MODULES", default=[], converter=CSV_TO_UNIQUE_LIST_CONVERTER
-    )
+    MJR_CONFIGURED_FACTORY_MODULES = MOJO_EXTENSION_VARIABLE_DEFAULTS.MJR_CONFIGURED_FACTORY_MODULES
+
 
 BRANDING_ESTABLISHED = False
 
@@ -60,8 +70,28 @@ def establish_rebranded_home(name: Optional[str]=None, home_directory: Optional[
     if not BRANDING_ESTABLISHED:
         BRANDING_ESTABLISHED = True
         if name is not None:
-            MOJO_EXTENSION_VARIABLES.MJR_NAME = name
+            MOJO_EXTENSION_VARIABLE_DEFAULTS.MJR_NAME = name
         if home_directory is not None:
-            MOJO_EXTENSION_VARIABLES.MJR_HOME_DIRECTORY = home_directory
+            MOJO_EXTENSION_VARIABLE_DEFAULTS.MJR_HOME_DIRECTORY = home_directory
+
+    return
+
+def resolve_extension_variables():
+    """
+        This method allows environment variables to be used to override configuration file and branding settings. This
+        is useful for integration.
+    """
+
+    MOJO_EXTENSION_VARIABLES.MJR_NAME = MOJO_EXTENSION_VARIABLE_DEFAULTS.MJR_NAME
+    if "MJR_NAME" in os.environ:
+        MOJO_EXTENSION_VARIABLES.MJR_NAME = os.environ["MJR_NAME"]
+
+    MOJO_EXTENSION_VARIABLES.MJR_HOME_DIRECTORY = MOJO_EXTENSION_VARIABLE_DEFAULTS.MJR_HOME_DIRECTORY
+    if "MJR_HOME_DIRECTORY" in os.environ:
+        MOJO_EXTENSION_VARIABLES.MJR_NAME = os.environ["MJR_HOME_DIRECTORY"]
+
+    MOJO_EXTENSION_VARIABLES.MJR_CONFIGURED_FACTORY_MODULES = MOJO_EXTENSION_VARIABLE_DEFAULTS.MJR_CONFIGURED_FACTORY_MODULES
+    if "MJR_CONFIGURED_FACTORY_MODULES" in os.environ:
+        MOJO_EXTENSION_VARIABLES.MJR_NAME = os.environ["MJR_CONFIGURED_FACTORY_MODULES"]
 
     return
