006ea6225ddd587fb8df5154481afbefd56b8238
Myron Walker <myron@Myrons-Mac-mini.local>
2024-03-11 11:15:03 -0700

    Put back the strip

--- source/packages/mojo/extension/wellknown.py
+++ source/packages/mojo/extension/wellknown.py
@@ -28,7 +28,7 @@ def ConfiguredSuperFactorySingleton():
     if CONFIGURED_SUPER_FACTORY is None:
         factory_modules = []
 
-        factory_modules_value = MOJO_EXTENSION_VARIABLES.MJR_CONFIGURED_FACTORY_MODULES
+        factory_modules_value = MOJO_EXTENSION_VARIABLES.MJR_CONFIGURED_FACTORY_MODULES.strip()
         if len(factory_modules_value) > 0:
             factory_modules = factory_modules_value.split(",")
         
