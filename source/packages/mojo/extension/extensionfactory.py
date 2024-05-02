5fb42f513d1f8b8fc73aed6b17ba5684a0b3a5c8
Myron Walker <myron@Myrons-Mac-mini.local>
2024-05-01 22:58:06 -0700

    Documentation updates

--- source/packages/mojo/extension/extensionfactory.py
+++ source/packages/mojo/extension/extensionfactory.py
@@ -7,6 +7,11 @@
 .. moduleauthor:: Myron Walker <myron.walker@gmail.com>
 """
 
+__author__ = "Myron Walker"
+__copyright__ = "Copyright 2023, Myron W Walker"
+__credits__ = []
+
+
 class ExtFactory:
     """
         The base class used to extend
